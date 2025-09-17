a1b84ba3d35e6011204f4282e563d79f31c34a9f
sweeppy <104524272+sweeppy@users.noreply.github.com>
2025-09-17 19:15:11 +0300

    Initial, create structure

---
+++ OrderService.API/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Order Service API",
+        Version = "v1"
+    });
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.Run();
+
